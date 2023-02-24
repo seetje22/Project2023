f05650b669514b783d684808a838bcf658391ab7
seetje22 <95631839+seetje22@users.noreply.github.com>
2023-02-24 01:00:50 -0800

    Add files via upload
    echo

---
+++ login_form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+</head>
+<body> 
+    <!--html formulier maken voor login-->
+    <form action="Login.php" method="post">
+        <input type="text" name="username" placeholder="Username"><br><br>
+        <input type="password" name="password" placeholder="Password"><br><br>
+        <input type="submit" value="Login">
+    </form>
+
+    <?php
+    
+    ?>
+
+</body>
+</html>
---
+++ register_form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>SignUp</title>
+</head>
+<body> 
+    <!--html formulier maken voor login-->
+    <form action="register_update.php" method="post">
+        <input type="test" name="email" placeholder="email"><br><br>
+        <input type="text" name="username" placeholder="Username"><br><br>
+        <input type="password" name="password" placeholder="Password"><br><br>
+        <input type="submit" value="SignUp">
+    </form>
+
+
+</body>
+</html>
